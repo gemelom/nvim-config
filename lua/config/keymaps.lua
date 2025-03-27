a2f9b457ea35ee4f2ce7cb8c1e220d8bb1b15af8
gemelom <3486609936@qq.com>
2025-03-27 19:03:28 +0800

    keymap: <leader>im to toggle input method

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,3 +4,13 @@ local opts = { noremap = true, silent = true }
 -- slower C-u and C-d
 keymap.set('n', '<C-u>', '15k')
 keymap.set('n', '<C-d>', '15j')
+
+-- toggle input method between English (1033) and Chinese (2052)
+keymap.set('n', '<leader>im', function()
+  local current_im = vim.fn.system 'im-select.exe'
+  if current_im:match '2052' then
+    vim.fn.system 'im-select.exe 1033'
+  else
+    vim.fn.system 'im-select.exe 2052'
+  end
+end, { desc = 'Toggle input method', noremap = true, silent = true })
