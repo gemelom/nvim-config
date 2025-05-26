3878fafae49f805e9e1683eb803a20c1fee7caf8
gemelom <3486609936@qq.com>
2025-05-26 15:08:36 +0800

    markdown drake theme

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -7,5 +7,11 @@ return {
     keys = {
       { '<leader>mp', '<cmd>MarkdownPreviewToggle<cr>', desc = 'Markdown Preview' },
     },
+    init = function()
+      vim.g.mkdp_preview_options = {
+        disable_filename = 1,
+      }
+      vim.g.mkdp_markdown_css = vim.fn.expand '~/.config/nvim/css/drake-vue.css'
+    end,
   },
 }
