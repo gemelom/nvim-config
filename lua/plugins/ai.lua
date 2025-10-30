e740b186f45c5c604c7ffabd0465b7cbd02f0f55
gemelom <3486609936@qq.com>
2025-10-30 14:32:50 +0800

    update

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -1,4 +1,29 @@
 return {
+  {
+    'zbirenbaum/copilot.lua',
+    cmd = 'Copilot',
+    build = ':Copilot auth',
+    event = 'BufReadPost',
+    opts = {
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        hide_during_completion = true,
+        keymap = {
+          accept = '<C-Enter>',
+          next = '<M-]>',
+          prev = '<M-[>',
+          -- dismiss = '<C-n>',
+        },
+      },
+      panel = { enabled = false },
+      filetypes = {
+        markdown = true,
+        help = true,
+        gitcommit = true,
+      },
+    },
+  },
   -- {
   --   'yetone/avante.nvim',
   --   event = 'VeryLazy',
